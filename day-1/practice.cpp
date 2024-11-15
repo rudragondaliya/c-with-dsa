43ff664db2f1ffcae3c7ab822814c5810d989d2d
rudragondaliya <gondaliyarudra61@gmail.com>
2024-11-15 18:00:11 +0530

    done

---
+++ day-1/practice.cpp
@@ -0,0 +1,129 @@
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+    // int a = 5; //intialization + declaration
+    // cout<<a<<endl;// print
+    // cout<<"hello world!";
+
+    // int a;
+    // cout<<"enter the value of a:";
+    // cin>>a;
+
+    // int a;
+    // int b;
+    // int c;
+
+    // cout<<"enter the value of a:";
+    // cin>>a;
+    // cout<<"enter the value of b:";
+    // cin>>b;
+
+    // c = a + b;
+    // cout<<"the sum is:"<<c;
+
+// }
+
+
+// basic code in c++ **********************************************
+
+// #include<iostream>
+// int main(){
+    
+//     std::cout<<"i like pizza!"<< std::endl;
+//     std::cout<<"it's really good!";
+//     return 0;
+// }
+
+
+// variables ***************************************************
+
+// #include<iostream>
+
+// int main(){
+
+    // int a;
+    // a = 12;
+
+    // std::cout<<a;
+
+    // int x = 5;
+    // int y = 6; 
+    // int z = x + y;
+
+    // std::cout<<x<<std::endl;
+    // std::cout<<y<<std::endl;
+    // std::cout<<z<<std::endl;
+
+    // return 0;
+
+
+// }
+
+// data types **************************************************
+
+
+// #include<iostream>
+
+// int main(){
+
+   // integer (whole number)*****************************************
+
+//    int age = 21;
+//    int year = 2020;
+//    int days = 4.4;
+
+//    std::cout<<days;
+
+// double (number including decimal) *************************************************
+
+// double price = 10.99;
+// double gpa = 2.4;
+// double temp = 25.1;
+
+// std::cout<<price;
+
+
+// single character *************************************************
+
+// char grade = 'A';
+// char initial = 'B';
+// char currency = '$';
+
+
+// std::cout<<initial;
+
+// boolean (true or false) *******************************************
+
+// bool student = false;
+// bool power = true;
+// bool forsale = true;
+
+// string (objects that represents a sequence of text) ******************************************
+
+// std::string name = "rudra";
+// std::string  day = "friday";
+// std::string food = "pizza";
+// std::string address = "123 fake city";
+
+
+// std::cout<< "hello "<<name<<'\n';
+// std::cout<<"you are "<<age<<" years old";
+
+
+// }
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // int a = 5;
+    // int b = 6;
+    // int c = a + b;
+
+    // cout<<c<<endl;
+
+    
+}
---
+++ day-1/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+double price = 10.99;
+// double gpa = 2.4;
+// double temp = 25.1;
+
+// std::cout<<price;
