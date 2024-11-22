8a27d757d7833a7b47a675944ec5b6c8864845d2
rudragondaliya <gondaliyarudra61@gmail.com>
2024-11-22 17:47:09 +0530

    done

---
+++ day-4/lab-work/que-3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   int n,i=1;
+ 
+   cout<<"enter any number:";
+   cin>>n;
+
+   while(i<=n){
+      cout<<i<<endl;
+       i++;
+   }
+   
+}
