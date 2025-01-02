201be341a0c0701895650f6665ea78186df78d6b
rudragondaliya <gondaliyarudra61@gmail.com>
2025-01-02 17:52:22 +0530

    done

---
+++ c++(projects)/circlet(project)/que-2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+ int i,j,num=11;
+ for(i=0;i<4;i++){
+    for(j=0;j<=i;j++){
+        cout<<num<<" ";
+        num++;
+    }
+    cout<<endl;
+ }
+    
+}
